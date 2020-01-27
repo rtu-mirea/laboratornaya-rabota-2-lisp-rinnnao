1aa992bf8a3c10117e2cd21daf7b8be07179304c
rinnnao <56185528+rinnnao@users.noreply.github.com>
2020-01-27 21:12:41 +0300

    Create Lab2.lisp

---
+++ Lab2.lisp
@@ -0,0 +1,1 @@
+;чтение файла (defun out (path) (let ((in (open path :if-does-not-exist nil))) (when in (loop for line = (read-line in nil) while line do (format t "~a~%" line)) (close in)))) (out ("Text.txt"))
