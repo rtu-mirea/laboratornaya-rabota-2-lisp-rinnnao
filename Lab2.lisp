0a05041e4b4c4ea399af5591e24f48cf38576c2d
rinnnao <56185528+rinnnao@users.noreply.github.com>
2020-02-01 12:18:36 +0300

    Delete Lab2.lisp

--- Lab2.lisp
+++
@@ -1,1 +0,0 @@
-;чтение файла (defun out (path) (let ((in (open path :if-does-not-exist nil))) (when in (loop for line = (read-line in nil) while line do (format t "~a~%" line)) (close in)))) (out ("Text.txt"))
